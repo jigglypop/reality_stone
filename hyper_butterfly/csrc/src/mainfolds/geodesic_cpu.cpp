4cc5a3e985fdcd58d96aa2e1d0bc2c2f0edf10db
jigglypop <donghwanyeom@gmail.com>
2025-05-06 02:55:48 +0900

    0506

--- hyper_butterfly/csrc/src/mainfolds/geodesic_cpu.cpp
+++ hyper_butterfly/csrc/src/mainfolds/geodesic_cpu.cpp
@@ -5,9 +5,9 @@
 namespace ops = hyper_butterfly::ops;
 
 namespace hyper_butterfly {
-namespace layers {
+namespace manifolds {
 
-torch::Tensor geodesic_forward_cpu(torch::Tensor u, torch::Tensor v, float c, float t) {
+torch::Tensor geodesic_cpu(torch::Tensor u, torch::Tensor v, float c, float t) {
     auto minus_u = ops::mobius_scalar_cpu(u, c, -1.0f);
     auto delta = ops::mobius_add_cpu(minus_u, v, c);
     auto delta_t = ops::mobius_scalar_cpu(delta, c, t);
